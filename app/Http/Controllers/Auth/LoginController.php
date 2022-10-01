7b99930c78a93f830afd2f7f6cf22bed4065ad15
eslamandroid12345 <eslamemo457@gmail.com>
2022-10-01 12:40:00 +0200

    update ogin

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -64,7 +64,7 @@ class LoginController extends Controller
     // Facebook callback
     public function handleFacebookCallback()
     {
-        $user = Socialite::driver('facebook')->user();
+        $user = Socialite::driver('facebook')->stateless()->user();
 
         $this->_registerOrLoginUser($user);
 
@@ -81,7 +81,7 @@ class LoginController extends Controller
     // Github callback
     public function handleGithubCallback()
     {
-        $user = Socialite::driver('github')->user();
+        $user = Socialite::driver('github')->stateless()->user();
 
         $this->_registerOrLoginUser($user);
 
